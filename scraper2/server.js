c221ee82250a991ac3bbb651746e284d47b6a55a
aleksficek <alex.ficek99@gmail.com>
2019-01-27 00:57:17 -0800

    Server

--- scraper2/server.js
+++ scraper2/server.js
@@ -7,7 +7,7 @@ const languagetool = require('languagetool-api');
 
 const params = {
   language: 'en-GB',
-  text: 'She are you doing',
+  text: 'She are you doing because we and if like like',
 };
 
 languagetool.check(params, function(err, res) {
@@ -15,12 +15,38 @@ languagetool.check(params, function(err, res) {
     console.log(err);
     console.log('HERE 1');
   } else {
-    console.log(res);
+    // console.log(res);
     languagetool.bestSuggestion(res, function(arr) {
-      console.log(arr);
+      var i = 0;
+      var offset_value = 0;
+      var length_value = 0;
+      var message_value = '';
+      var sentence_value = res.matches[0].sentence;
+      var spacing = '';
+      var addition = 0;
+      console.log(sentence_value);
+
       arr.forEach(function(item) {
-        console.log('HERE 2');
+        offset_value = res.matches[i].offset;
+        console.log(offset_value);
+
+        length_value = res.matches[i].length;
+        console.log(length_value);
+
+        message_value = res.matches[i].message;
+        console.log(message_value);
+
+        if (i > 0) {
+          spacing = ' '
+          addition = 1;
+        }
+        sentence_value = sentence_value.slice(0, offset_value - addition) + item.bestSuggestion + spacing +
+        sentence_value.slice(offset_value + length_value);
+
+        console.log(sentence_value);
+
         console.log('Mistake: '+item.mistake+'Suggest: '+item.bestSuggestion);
+        i++;
       });
     });
   };
